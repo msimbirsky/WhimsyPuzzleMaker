14d840df05268a20f885c9e2eb2b2fa0e309c8fc
Bozhie Pokorny <imbozhie@gmail.com>
2024-03-01 16:11:44 -0800

    first test

--- Process/line_warpers.py
+++ Process/line_warpers.py
@@ -1,6 +1,25 @@
 """Module for and generating altering line segments to assist processing and defining puzzle pieces."""
 
 import numpy as np
+import math
+
+def rotate(origin, point, angle):
+    """
+    Rotate a point counterclockwise by a given angle around a given origin.
+    From: https://stackoverflow.com/questions/34372480/rotate-point-about-another-point-in-degrees-python
+
+    The angle should be given in radians.
+    """
+    ox, oy = origin
+    px, py = point
+
+    qx = ox + math.cos(angle) * (px - ox) - math.sin(angle) * (py - oy)
+    qy = oy + math.sin(angle) * (px - ox) + math.cos(angle) * (py - oy)
+    return qx, qy
+
+def midpoint(p1, p2):
+    """"Calculate the midpoint between two points."""
+    return (p1[0] + p2[0]) / 2, (p1[1] + p2[1]) / 2
 
 
 def generate_interlocking_segment(start_coord, stop_coord, neck_width, neck_height, head_width, head_height) -> list[tuple]:
@@ -18,21 +37,27 @@ def generate_interlocking_segment(start_coord, stop_coord, neck_width, neck_heig
         list[tuple]: A list of coordinates (x, y) that define a curve for the interlocking segment.
     """
 
-    # Calculate the slope of the line between the start and stop coordinates
-    slope = (stop_coord[1] - start_coord[1]) / (stop_coord[0] - start_coord[0])
-
-    # Calculate the angle of the line between the start and stop coordinates
-    angle = np.arctan(slope)
-
     anchor_points = [None] * 10
     anchor_points[0] = start_coord
     anchor_points[9] = stop_coord
+
     # Calculate the coordinates of the neck and head of the puzzle piece
-    anchor_points[1] = (start_coord[0] - np.cos(angle), start_coord[1] + neck_height)
-    neck_coord = (start_coord[0] + neck_width * np.cos(angle), start_coord[1] + neck_height * np.sin(angle))
-    head_coord = (stop_coord[0] - head_width * np.cos(angle), stop_coord[1] - head_height * np.sin(angle))
+    anchor_points[1] = (start_coord[0], start_coord[1] + neck_height)
+    anchor_points[2] = (start_coord[0] - (0.5*head_width - 0.5*neck_width), 
+                        anchor_points[1][1])
+    anchor_points[3] = (anchor_points[2][0], 
+                        (anchor_points[2][1] + 0.5*head_height))
+    anchor_points[4] = (anchor_points[3][0], 
+                        (anchor_points[3][1] + 0.5*head_height))
+    anchor_points[5] = (anchor_points[4][0] + head_width, anchor_points[4][1])
+    anchor_points[6] = (anchor_points[5][0], (anchor_points[5][1] - 0.5*head_height))
+    anchor_points[7] = (anchor_points[6][0], (anchor_points[6][1] - 0.5*head_height))
+    anchor_points[8] = (anchor_points[7][0] - (0.5*head_width - 0.5*neck_width), 
+                        anchor_points[7][1])
 
-    # Generate the interlocking segment
-    interlocking_segment = [start_coord, neck_coord, head_coord, stop_coord]
+    # Rotate the coordinates to align with the base line
+    origin = midpoint(start_coord, stop_coord)
+    angle = math.atan2(stop_coord[1] - start_coord[1], stop_coord[0] - start_coord[0])
+    anchor_points = [rotate(origin, point, angle) for point in anchor_points]
 
-    return interlocking_segment
+    return anchor_points
--- tests/test_puzzle_piece_creation.py
+++ tests/test_puzzle_piece_creation.py
@@ -5,13 +5,13 @@ from Process.line_warpers import generate_interlocking_segment
 def test_generate_interlocking_segment():
     """Test the generate_interlocking_segment function."""
 
-    expected_coords = [(10, 0), (10, 20), (0, 20), (0, 60), (0, 100), (30, 100), (30, 60), (30, 20), (20, 20), (20, 0)]
+    expected_coords = [(10., 0.), (10., 20.), (0., 20.), (0., 60.), (0., 100.), (30., 100.), (30., 60.), (30., 20.), (20., 20.), (20., 0.)]
     start_coord = (10, 0)
     stop_coord = (20, 0)
     neck_width = 10
     neck_height = 20
     head_width = 30
-    head_height = 60
+    head_height = 80
 
     interlocking_segment = generate_interlocking_segment(start_coord, stop_coord, neck_width, neck_height, head_width, head_height)
     assert interlocking_segment == expected_coords
